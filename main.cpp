c7be6680235e2f85658ef8c8d32b26bb8601ae15
Ryo-99 <sh24279b@st.omu.ac.jp>
2024-05-01 12:13:48 +0000

    頂点検知のため編集中

---
+++ main.cpp
@@ -0,0 +1,165 @@
+#include "mbed.h"
+
+#include "BNO055.h"
+#include "BME280.h"
+
+//PCへのシリアル通信
+Serial pc(USBTX, USBRX, 9600);
+
+//mbed LED1
+DigitalOut led(LED1);
+
+//両方のセンサーに使用するI2C
+I2C ifaceI2C(I2C_SDA, I2C_SCL);
+
+//両方のセンサーをI2Cで使用する
+BOARDC_BNO055 sensor1(&ifaceI2C);
+BOARDC_BME280 sensor2(&ifaceI2C);
+
+int main(){
+
+    //挨拶表示
+    printf("mbed READY\r\n");
+
+    wait_ms(1000);
+    led = 1;
+
+    printf("Start Comm - -- ---- --------\r\n");
+
+    //I2Cインターフェースで２つのセンサーを使用する(200KHz)
+    ifaceI2C.frequency(100000);
+    sensor1.initialize(false);
+    sensor2.initialize(false);
+/*
+    //各センサーのチップIDなどを表示するステートメント
+    {
+        char chipID = sensor1.getChipID();
+        char AccChipID = sensor1.getAccChipID();
+        char MagChipID = sensor1.getMagChipID();
+        char GyroChipID = sensor1.getGyroChipID();
+        char bootLoader = sensor1.getBootRevision();
+
+        char bme280chip = sensor2.getChipID();
+
+        printf("BNO055 (9DOF sensor) ---- ---- ---- ---- ---- ---- ---- ---- ----\r\n");
+        printf("chipID = 0x%02X, bootLoader = %d\r\n", chipID, bootLoader);
+        printf("Acc    = 0x%02X\r\nMag    = 0x%02X\r\nGyro   = 0x%02X\r\n\r\n", AccChipID, MagChipID, GyroChipID);
+
+        printf("BME280 (Temperature, Humidity, Pressure sensor) -- ---- ---- ----\r\n");
+        printf("chipID = 0x%02X\r\n\r\n", bme280chip);
+
+    }
+
+    wait_ms(1000);
+    led = 0;
+
+    //各センサーの現在の設定などを表示するステートメント
+    {
+        char mode = sensor1.getOperationMode();
+        char sysStatus = sensor1.getSystemStatus();
+        char testStatus = sensor1.getSelfTestResultAll();
+        char errorStatus = sensor1.getSystemError();
+        char calibST[4];
+        sensor1.getCalibStatusAll(calibST[0], calibST[1], calibST[2], calibST[3]);
+
+        char bme280mode_hum = sensor2.getCTRL_humidity();
+        char bme280mode_meas = sensor2.getCTRL_measuring();
+        char bme280mode_conf = sensor2.getConfig();
+
+        printf("BNO055 (9DOF sensor) ---- ---- ---- ---- ---- ---- ---- ---- ----\r\n");
+        printf("Mode = 0x%02X, sysStatus = 0x%02X, testStatus = 0x%02X\r\n", mode, sysStatus, testStatus);
+        printf("calibSys = %d [%%], calibAcc = %d [%%], calibMag = %d [%%], calibGyro = %d [%%]\r\n", calibST[0], calibST[1], calibST[2], calibST[3]);
+        printf("errorStatus = 0x%02X, err = 0x%02X, len = %d\r\n\r\n", errorStatus, sensor1.getIfaceLastError(), sensor1.getIfaceLastLength());
+
+        printf("BME280 (Temperature, Humidity, Pressure sensor) -- ---- ---- ----\r\n");
+        printf("CTRL_HUM = 0x%02X, CTRL_MEAS = 0x%02X, CONFIG = 0x%02X\r\n\r\n", bme280mode_hum, bme280mode_meas, bme280mode_conf);
+    }*/
+
+    wait_ms(1000);
+    led = 1;
+
+    //各センサーの値を格納するための変数宣言
+    short dataBox[12];
+    float scAcc, scMag, scGyro, scEUL, scTemp;
+    float ax, ay, az, mx, my, mz, gx, gy, gz, temp;
+    double yaw, roll, pitch;
+    float bme280_T = 0.0, bme280_P = 0.0, bme280_H = 0.0;
+    char bme280_status = 0x00;
+
+    //センサーのRAW値を実際の数値に変換するための倍率を取得する
+    scAcc = sensor1.getAccScale();
+    scMag = sensor1.getMagScale();
+    scGyro = sensor1.getGyroScale();
+    scEUL = sensor1.getEulerScale();
+    scTemp = sensor1.getTempScale();
+
+    //ボタンが押されるまで繰り返し続ける(Nucleo専用)
+    //ボタンがない場合はwhile(1)の無限ループで代用
+    while(1){
+        //配列dataBoxに、9軸の値とオイラー角(yaw roll pitch)を格納(計12個の値)
+        sensor1.get9AxisAndEUL(dataBox);
+
+        //倍率をかけてRaw値を実際の値に変換
+        ax = (float)dataBox[0] * scAcc;
+        ay = (float)dataBox[1] * scAcc;
+        az = (float)dataBox[2] * scAcc;
+        mx = (float)dataBox[3] * scMag;
+        my = (float)dataBox[4] * scMag;
+        mz = (float)dataBox[5] * scMag;
+        gx = (float)dataBox[6] * scGyro;
+        gy = (float)dataBox[7] * scGyro;
+        gz = (float)dataBox[8] * scGyro;
+        /*yaw = (float)dataBox[9] * scEUL;
+        roll = (float)dataBox[10] * scEUL;
+        pitch = (float)dataBox[11] * scEUL;*/
+
+        sensor1.getEulerFromQ(yaw,roll,pitch);
+
+        //BNO055内のセンサーの参考温度を取得して実際の値に変換
+        temp = (float)sensor1.getTemperature() * scTemp;
+
+        //温湿度センサーより、温度、湿度、気圧、現在の状態を取得して変数に格納
+        bme280_T = sensor2.getTemp();
+        bme280_P = sensor2.getPress_hPa();
+        bme280_H = sensor2.getHum();
+        bme280_status = sensor2.getStatus();
+
+        //温湿度センサーの補正データが更新されていたなら、計算用数値を更新
+        if(sensor2.isReady()){
+            sensor2.updateCalib();
+        }
+
+        printf(
+            "Acc = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\nMag = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\nGyr = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\n",
+            ax, ay, az, mx, my, mz, gx, gy, gz
+        );
+
+        printf("%lf %lf %lf\r\n",roll,pitch,yaw);
+
+        printf(
+            "Temperature\t = %03.3f[degC] (BNO055 -> %03.3f[degC])\r\nPressure\t = %06.3f[hPa]\r\nHumidity\t = %03.3f[%%RH]\r\nStatus\t = 0x%02X\r\n",
+            bme280_T, temp, bme280_P, bme280_H, bme280_status
+        );
+         
+        double height = (pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15)/0.0065;
+        printf("Pressure\t = %06.3f[hPa]\r\nHeight\t = %06.3f[m]\r\n",bme280_P,height);
+        wait_ms(10);
+        
+        /*double previous_height = 0;
+        int n=0, m=0;
+        n=height-previous_height;
+        for(int i=0;i<100000;i++){
+            if(n<0){
+                m++;
+            }
+            else{
+                m=0;
+            }
+            if(m==5){
+                printf("相対高度が５回負");
+            }
+            previous_height=height;
+            //wait_ms(10);
+        }*/
+    }
+}
