3794daa7678c6c6443ef377923503450cbfd9229
Haruto-8610 <si23537d@st.omu.ac.jp>
2024-05-13 06:37:50 +0000

    previous_heightをprintfしないようにしたら、正常な値を吐き出すようになった

--- main.cpp
+++ main.cpp
@@ -167,12 +167,16 @@ void peakcheck(){
     double n= height-previous_height;//相対高度 
     //printf("m=%d\r\nPressure=%f\r\nTemperature=%f\r\nHeight=%f[m]\r\nPrevious_height=%f\r\nn=%f\r\n",m,bme280_P,bme280_T,height,previous_height,n);
     //printf("m=f%d\r\npow(1013.25/bme280_p , 1/5.257)=%f\r\n(pow(1013.25/bme_280_P , 1/5.257)-1)*(bme280_T+273.15)=%f\r\n(pow(1013.25/bme_280_P , 1/5.257)-1)*(bme280_T+273.15)/0.0065=%f\r\nheight=%f\r\n",m,pow(1013.25/bme280_P , 1/5.257),(pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15),((pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15))/0.0065,height);
+    
     printf("pressure=%f\r\n",bme280_P);
     printf("temperature=%f\r\n",bme280_T);
     printf("height=%f\r\n",height);
     //printf("previous_height=%f\r\n",previous_height);
     printf("n=%f\r\n",n);
-    //printf("m=%d\r\n",m);
+    printf("m=%d\r\n",m);
+
+    //↑previous_heightもprintfすると、値が変化しなくなる
+
     if(n<0){
         m++;
     }
