d5e41cbec59b8a575b5d39dee64ffa4600464d2b
Haruto-8610 <si23537d@st.omu.ac.jp>
2024-08-20 07:39:04 +0000

    頂点検知組み込み

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 #include "BME280.h"
 #include <cstdio>
 
+Ticker peakchecktime;
 
-Serial pc(USBTX, USBRX, 115200);
+void peakcheck();
+
+Serial pc(USBTX, USBRX, 9600);
+
+//mbed LED1
+DigitalOut led(LED1);
 
 SDFileSystem sd(PA_7, PA_6, PA_5, PB_0, "sd");
 DigitalIn sdcheck(D12);
@@ -14,10 +20,21 @@ I2C ifaceI2C(I2C_SDA, I2C_SCL);
 BOARDC_BNO055 sensor1(&ifaceI2C);
 BOARDC_BME280 sensor2(&ifaceI2C);
 
+DigitalIn M2S(PA_8);
+DigitalOut S2M(PA_11);
 
 Timer t;
 
+double bme280_P = 0.0;
+double bme280_T = 0.0;
+//double height = ((pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15))/0.0065;
+double previous_height = 0.0;
+//double n = height-previous_height;//相対高度 
+int m = 0;//相対高度が連続して負になった回数
+
 int main() {
+    if(M2S == 1){
+
     int sdmode =sdcheck;
     printf("SDmode= %d \r\n",sdmode); //0ならスロットイン
     mkdir("/sd/mydir100", 0777);
@@ -28,11 +45,11 @@ int main() {
     sensor2.initialize(false);
     //各センサーの値を格納するための変数宣言
     short dataBox[12];
-    float scAcc, scMag, scGyro, scEUL, scTemp;
+    float scAcc, scMag, scGyro, scEUL, scTemp, scLIA; //scLIA追加
     float ax, ay, az, mx, my, mz, gx, gy, gz, temp;
     short L_accX, L_accY, L_accZ;
     double yaw, roll, pitch;
-    float bme280_T = 0.0, bme280_P = 0.0, bme280_H = 0.0;
+    float /*bme280_T = 0.0, bme280_P = 0.0,*/ bme280_H = 0.0;
     char bme280_status = 0x00;
     //センサーのRAW値を実際の数値に変換するための倍率を取得する
     scAcc = sensor1.getAccScale();
@@ -40,11 +57,12 @@ int main() {
     scGyro = sensor1.getGyroScale();
     scEUL = sensor1.getEulerScale();
     scTemp = sensor1.getTempScale();
+    scLIA = sensor1.getLinearScale(); 
 
     t.start();
     int time;
 
-    while(time < 10) {
+    while(/*time < 10*/1) {
         //配列dataBoxに、9軸の値とオイラー角(yaw roll pitch)を格納(計12個の値)
         sensor1.get9AxisAndEUL(dataBox);
         //倍率をかけてRaw値を実際の値に変換
@@ -57,9 +75,11 @@ int main() {
         gx = (float)dataBox[6] * scGyro;
         gy = (float)dataBox[7] * scGyro;
         gz = (float)dataBox[8] * scGyro;
-        /*yaw = (float)dataBox[9] * scEUL;
+        /*
+        yaw = (float)dataBox[9] * scEUL;
         roll = (float)dataBox[10] * scEUL;
-        pitch = (float)dataBox[11] * scEUL;*/
+        pitch = (float)dataBox[11] * scEUL;
+        */
         sensor1.getEulerFromQ(yaw,roll,pitch);
         //BNO055内のセンサーの参考温度を取得して実際の値に変換
         temp = (float)sensor1.getTemperature() * scTemp;
@@ -73,24 +93,37 @@ int main() {
             sensor2.updateCalib();
         }
         //printf("Acc = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\nMag = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\n",ax, ay, az, mx, my, mz);
-
+        
         printf("pitch= %lf roll= %lf yaw= %lf\r\n",roll,pitch,yaw);//yawの所がrollになっている
         fprintf(fp,"pitch= %lf roll= %lf yaw= %lf\r\n",roll,pitch,yaw);
+        
         //printf("Temperature\t = %03.3f[degC] (BNO055 -> %03.3f[degC])\r\nPressure\t = %06.3f[hPa]\r\nHumidity\t = %03.3f[%%RH]\r\nStatus\t = 0x%02X\r\n",bme280_T, temp, bme280_P, bme280_H, bme280_status);
 
         //fprintf(fp,"Acc = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\nMag = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\n pitch= %lf roll= %lf yaw= %lf\r\nTemperature\t = %03.3f[degC] (BNO055 -> %03.3f[degC])\r\nPressure\t = %06.3f[hPa]\r\nHumidity\t = %03.3f[%%RH]\r\nStatus\t = 0x%02X\r\n",ax, ay, az, mx, my, mz,roll,pitch,yaw,bme280_T, temp, bme280_P, bme280_H, bme280_status);
         
         //線形加速度を取得//線形加速度は重力の影響が除去されており、デバイスの運動を知りたいときに見るらしい？
-        sensor1.getLinearAccDataAll(L_accX, L_accY, L_accZ);
-
-        printf("Acc = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\n",L_accX, L_accY, L_accZ);
+        
+        /*
+        short a =sensor1.getLinearAccDataX();
+        short b =sensor1.getLinearAccDataY();
+        short c =sensor1.getLinearAccDataZ();
+        printf("b=%f, a=%f, c=%f\r\n",b,a,c);
+        */
+        
+        //sensor1.getLinearAccDataAll(L_accX, L_accY, L_accZ)
+        L_accX = sensor1.getLinearAccDataX() * scLIA;
+        L_accY = sensor1.getLinearAccDataY() * scLIA;
+        L_accZ = sensor1.getLinearAccDataZ() * scLIA;
+        
+        printf("Acc = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\n", L_accX, L_accY, L_accZ);
         fprintf(fp,"Acc = X[%06.5f], Y[%06.5f], Z[%06.5f]\r\n",L_accX, L_accY, L_accZ);
+    
         time = t.read();
     }
     fclose(fp); 
 
     //.txtファイル読み込み
-     fp = fopen("/sd/mydir100/sdtest.txt", "r");
+    fp = fopen("/sd/mydir100/sdtest.txt", "r");
     if(fp == NULL) {
         pc.printf("Could not open file for write\n");
     }
@@ -100,4 +133,36 @@ int main() {
         pc.printf("%s", buffer);
     }
     fclose(fp); 
+    }
+}
+
+
+void peakcheck(){
+    double height = ((pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15))/0.0065;//外に出すとinfと表示される。原因は謎
+    double n=height-previous_height;//相対高度 
+    //printf("m=%d\r\nPressure=%f\r\nTemperature=%f\r\nHeight=%f[m]\r\nPrevious_height=%f\r\nn=%f\r\n",m,bme280_P,bme280_T,height,previous_height,n);
+    //printf("m=f%d\r\npow(1013.25/bme280_p , 1/5.257)=%f\r\n(pow(1013.25/bme_280_P , 1/5.257)-1)*(bme280_T+273.15)=%f\r\n(pow(1013.25/bme_280_P , 1/5.257)-1)*(bme280_T+273.15)/0.0065=%f\r\nheight=%f\r\n",m,pow(1013.25/bme280_P , 1/5.257),(pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15),((pow(1013.25/bme280_P , 1/5.257)-1)*(bme280_T+273.15))/0.0065,height);
+    
+   /* printf("pressure=%f\r\n",bme280_P);
+    printf("temperature=%f\r\n",bme280_T);
+    printf("height=%f\r\n",height);
+    printf("n=%f\r\n",n);
+    printf("m=%d\r\n",m);
+    */
+
+    if(n<-0.1){
+        m++;
+    }
+    else{
+        m=0;//カウントリセット
+    }
+
+    printf("%f,%f,%d\r\n",bme280_P,t.read(),m);
+
+    if(m==5){
+        S2M = 1;//頂点検知をsubからmainへ送る
+        peakchecktime.detach();
+        printf("頂点検知成功\n");
+    }
+    previous_height = height;
 }
